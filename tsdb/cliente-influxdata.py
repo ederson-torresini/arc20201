d1c7a57140e2172027f162840cafef144af1ab5e
Ederson Torresini <ederson@boidacarapreta.cc>
2020-05-29 13:49:47 +0000

    Configurar variáveis de ambiente

--- tsdb/cliente-influxdata.py
+++ tsdb/cliente-influxdata.py
@@ -7,7 +7,7 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 # Variáveis do servidor InfluxDB
 token = os.environ.get('INFLUXDB_TOKEN')
 org = os.environ.get('INFLUXDB_ORG')
-bucket = "ideias"
+bucket = os.environ.get('INFLUXDB_BUCKET')
 
 # Testar se as variáveis de ambiente foram criadas
 if token is None or org is None:
@@ -16,11 +16,11 @@ if token is None or org is None:
 
 # Criar os objetos da consulta
 client = InfluxDBClient(
-    url="https://us-central1-1.gcp.cloud2.influxdata.com", token=token)
+    url=os.environ.get('INFLUXDB_BASEURL'), token=token)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 # Construir os dados a serem escritos no banco
-data = "ideia,local=casa,personagem=Matias,ação=lendo latitude=-27.6084177,longitude=-48.6354691,altitude=80"
+data = "ideia,local=sonho,personagem=Lola,ação=voando latitude=-27.6084177,longitude=-48.6354691,altitude=80"
 
 # Realizar, de fato, a operação de escrita no banco de dados
 write_api.write(bucket, org, data)
