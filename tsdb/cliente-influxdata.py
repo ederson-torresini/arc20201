19bb76819cf8a6f7cb51a65532fd74f48b058902
Ederson Torresini <ederson@boidacarapreta.cc>
2020-06-05 12:57:39 +0000

    Usar Python + requests + InfluxDB

--- tsdb/cliente-influxdata.py
+++ tsdb/cliente-influxdata.py
@@ -14,7 +14,7 @@ client = InfluxDBClient(url=environ.get('INFLUXDB_BASEURL'), token=token)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 # Construir os dados a serem escritos no banco
-data = "ideia,local=sonho,personagem=Lola,ação=voando latitude=-27.6084177,longitude=-48.6354691,altitude=80"
+data = "ideia,local=quarto,personagem=Lola,acao=curiosa latitude=-27.6084177,longitude=-48.6354691,altitude=80"
 
 # Realizar, de fato, a operação de escrita no banco de dados
 write_api.write(bucket, org, data)
---
+++ tsdb/cliente-requests.py
@@ -0,0 +1,21 @@
+# https://requests.readthedocs.io/pt_BR/latest/
+
+# Carregar módulos
+from os import environ
+import requests
+import json
+
+# Criar variáveis do servidor InfluxDB
+baseurl = environ.get('INFLUXDB_BASEURL')
+token = environ.get('INFLUXDB_TOKEN')
+org = environ.get('INFLUXDB_ORG')
+bucket = environ.get('INFLUXDB_BUCKET')
+
+# Formatar a consulta
+url = "{}/api/v2/write?org={}&bucket={}".format(baseurl, org, bucket)
+cabeçalhos = {"Authorization": "Token {}".format(token)}
+dados = "ideia,local=quarto,personagem=Lola,acao=curiosa latitude=-27.6084177,longitude=-48.6354691,altitude=80"
+
+# Criar os objetos da consulta e realizar, de fato, a operação de escrita no banco de dados
+req = requests.post(url, headers=cabeçalhos, data=dados)
+print(req.status_code)
