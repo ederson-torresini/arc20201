c22412d62899b5ae2c8fc77194b617f966ef19fe
Ederson Torresini <ederson@boidacarapreta.cc>
2020-07-04 03:04:35 +0000

    Remover servidor HTTP desnecessário
    O servidor HTTP já está em desenvolvimento em tsdb/.

--- http/servidor.py
+++
@@ -1,9 +0,0 @@
-# Obtido de https://flask.palletsprojects.com/en/1.1.x/quickstart/#a-minimal-application
-
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
