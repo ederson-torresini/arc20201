255d690e32570a73c4d23282c4f5b92e6353146c
Ederson Torresini <ederson@boidacarapreta.cc>
2020-08-29 00:20:47 -0300

    Iniciar estudos com Hubot

--- skills/hi/__init__.py
+++
@@ -1,8 +0,0 @@
-from opsdroid.skill import Skill
-from opsdroid.matchers import match_regex
-
-
-class HelloSkill(Skill):
-    @match_regex(r'hi', case_sensitive=False)
-    async def hello(self, message):
-        await message.respond('Hello, ' + message.user)
